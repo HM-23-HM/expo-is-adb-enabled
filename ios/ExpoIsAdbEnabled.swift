7e02d4de3bd89b9395397938d5421676730a0305
Hodeem Miller <hodeem@Hodeems-MacBook-Pro.local>
2024-02-15 14:44:08 -0500

    Changed the Name back to ExpoIsAdbEnabled

--- ios/ExpoIsAdbEnabled.swift
+++ ios/ExpoIsAdbEnabled.swift
@@ -8,7 +8,7 @@ public class ExpoIsAdbEnabledModule: Module {
     // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
     // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
     // The module will be accessible from `requireNativeModule('ExpoIsAdbEnabled')` in JavaScript.
-    Name("ExpoIsAdbEnabledModule")
+    Name("ExpoIsAdbEnabled")
 
     // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
     Function("isAdbEnabled") {
